75aa42b5fa228c63832eeaf7e7696682bc86f255
Mikalai <kazlou.mikalai.s@gmail.com>
2025-01-16 09:33:00 +0300

    fix: change the path of text file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,21 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-
-const pathToFile = path.format({
-    root: '/',
-    dir: '01-read-file',
-    base: 'text.txt',
-});
+const pathToFile = path.join(__dirname, 'text.txt');
 
 function getTextFromFile(path) {
   if (!path) {
     return console.log('Path is not valid or empty');
   }
   let text = fs.createReadStream(path);
-  text.on('error', (error)=>{
-    console.log('***Exception is threw into getTextFromFile function***\n' + error);
+  text.on('error', (error) => {
+    console.log('Exception: ' + error);
   });
-  text.on('data', (data)=>{
+  text.on('data', (data) => {
     console.log(data.toString());
   });
 }
