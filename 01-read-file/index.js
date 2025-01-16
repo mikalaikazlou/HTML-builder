45b69907a822f89c823b25ed7e844050f91b1496
Mikalai <kazlou.mikalai.s@gmail.com>
2025-01-16 15:28:12 +0300

    fix: change work of getting of text from file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 const pathToFile = path.join(__dirname, 'text.txt');
+let data = '';
 
-function getTextFromFile(path) {
-  if (!path) {
-    return console.log('Path is not valid or empty');
-  }
-  let text = fs.createReadStream(path);
-  text.on('error', (error) => {
-    console.log('Exception: ' + error);
-  });
-  text.on('data', (data) => {
-    console.log(data.toString());
-  });
-}
+let stream = fs.createReadStream(pathToFile);
+stream.setEncoding('utf-8');
 
-getTextFromFile(pathToFile);
+stream.on('error', (error) => {
+  console.log('Exception: ' + error);
+});
+stream.on('data', (dataPart) => {
+  data += dataPart;
+});
+stream.on('end', () => {
+  console.log(data);
+  stream.destroy();
+});
